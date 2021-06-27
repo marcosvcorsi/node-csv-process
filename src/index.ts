ff2b6d43aad4ff94efcf3115af31a1b6951f6f9d
Marcos Corsi <marcosvcorsi@gmail.com>
2021-06-27 02:20:42 -0300

    feat: test completed

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
-import { createReadStream } from 'fs';
+import { createReadStream, createWriteStream } from 'fs';
 import { resolve } from 'path';
+import { Transform, pipeline, Writable } from 'stream';
+import { promisify } from 'util';
 import csvtojson from 'csvtojson';
+import loki from 'lokijs';
+import { v4 } from 'uuid';
+
+const db = new loki('bitcoins');
+
+var prices = db.addCollection('prices', { indices: ['id'] });
 
 type BitcoinRow = {
   Date: string;
@@ -11,6 +19,35 @@ type Bitcoin = {
   value: number;
   date?: Date;
 }
+
+const pipelineAsync = promisify(pipeline);
+
+const writeData = () => {
+  return new Writable({
+    write: async (chunk, _, cb) => {
+      const json = chunk.toString();
+      const item = JSON.parse(json);
+
+      prices.insert({
+        id: v4(),
+        ...item,
+      });
+
+      cb();
+    }
+  })
+}
+
+const mapStream = () => {
+  return new Transform({
+    objectMode: true,
+    transform: async (chunk, _, cb) => {
+      const item = chunk.toString();
+
+      cb(null, item);
+    },
+  })
+} 
 
 (async () => {
   const stream = createReadStream(resolve(__dirname, '..', 'data', 'bitcoins.csv'));
@@ -39,5 +76,21 @@ type Bitcoin = {
     }
   });
 
-  console.table([highest, lowest])
+  console.table([highest, lowest]);
+
+  const readStream = createReadStream(resolve(__dirname, '..', 'data', 'bitcoins.csv'));
+
+  await pipelineAsync(
+    readStream,
+    csvtojson(),
+    mapStream(),
+    writeData()
+  );
+
+  const priceList = prices
+    .chain()
+    .find()
+    .data();
+
+  console.log('prices', priceList);
 })();
